c230b871bce4cf0caa26d161400fca7589d5cb8f
Florent Pastor <florent.pastor@ostmodern.co.uk>
2019-02-02 12:47:30 +0100

    fixup! Can now navigate with left/right arrows

---
+++ src/cljs/cornelius_reader/dispatcher.cljs
@@ -0,0 +1,9 @@
+(ns cornelius-reader.dispatcher
+  (:require [re-pressed.core :as rp]
+            [re-frame.core :as re-frame]))
+
+(defn create-event-dispatchers
+  []
+  (re-frame/dispatch-sync [::rp/set-keydown-rules {:event-keys [[[:cornelius-reader.events/previous-page] [{:keyCode 37}]]
+                                                                [[:cornelius-reader.events/following-page] [{:keyCode 39}]]]}])
+  (re-frame/dispatch-sync [::rp/add-keyboard-event-listener "keydown"]))
